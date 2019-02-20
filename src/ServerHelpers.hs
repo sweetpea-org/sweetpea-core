c7c473483a99aecdd094ef1eb7433698df64feb4
Ben Draut <drautb@gmail.com>
2019-02-19 21:46:22 -0700

    Add a few more helper functions
    These will be useful for implementing the new approach I want to try.

--- src/ServerHelpers.hs
+++ src/ServerHelpers.hs
@@ -3,14 +3,42 @@ module ServerHelpers where
 import Data.List
 import Data.Text (Text, pack, unpack, split, strip)
 
+import DataStructures
+
+
 strToInt :: Text -> Int
 strToInt str = read $ unpack str
 
-updateHeader :: String -> String
-updateHeader header =
+
+addClauseToHeader :: String -> String
+addClauseToHeader = updateHeader 1
+
+
+updateHeader :: Int -> String -> String
+updateHeader additionalClauseCount header =
   let segments = split (==' ') (strip (pack header))
-  in let newClauseCount = (strToInt (segments !! 3)) + 1
+  in let newClauseCount = (strToInt (segments !! 3)) + additionalClauseCount
      in unwords $ map unpack ((take 3 segments) ++ [pack $ show newClauseCount])
+
+
+cnfToLine :: [Int] -> String
+cnfToLine cnf =
+  intercalate " " ((map show cnf) ++ ["0"])
+
+
+cnfsToLines :: CNF -> [String]
+cnfsToLines cnfs =
+  map cnfToLine cnfs
+
+
+-- Appends a set of clauses to an existing CNF string, updates the header, and returns the new CNF string.
+addClausesToCnf :: String -> CNF -> String
+addClausesToCnf cnfStr cnfs =
+  let lines = split (=='\n') (strip $ pack cnfStr) in
+    let updatedHeader = updateHeader (length cnfs) (unpack $ lines !! 0) in
+      let updatedLines = [(pack updatedHeader)] ++ (drop 1 lines) ++ (map pack (cnfsToLines cnfs)) in
+        intercalate "\n" (map unpack updatedLines)
+
 
 parseCMSatSolution :: String -> [Int]
 parseCMSatSolution output = do
@@ -21,3 +49,7 @@ parseCMSatSolution output = do
     else let lines = filter (isPrefixOf "v") (map (unpack . strip) (split (=='\n') (strip (pack output))))
          in let intStrs = split (==' ') $ strip $ pack $ (filter (/='v') (foldr (++) "" lines))
             in map strToInt intStrs
+
+
+isFormulaSat :: String -> Bool
+isFormulaSat cmSatOutput = isInfixOf "s SATISFIABLE" cmSatOutput
--- test/Spec.hs
+++ test/Spec.hs
@@ -17,7 +17,15 @@ dataStructureTests :: TestTree
 dataStructureTests = testGroup "DataStructure Tests" [helperTests]
 
 serverTests :: TestTree
-serverTests = testGroup "Server Tests" [strToIntTests, updateHeaderTests, parseCMSatSolutionTests]
+serverTests = testGroup "Server Tests"
+  [ strToIntTests
+  , addClauseToHeaderTests
+  , updateHeaderTests
+  , cnfsToLinesTests
+  , addClausesToCnfTests
+  , parseCMSatSolutionTests
+  , isFormulaSatTests
+  ]
 
 ---------------------------------------------------------------------------------------------------------------
 -- DataStructures tests
@@ -60,9 +68,32 @@ strToIntTests = testGroup "strToInt tests"
     strToInt "42" @?= 42
   ]
 
-updateHeaderTests = testGroup "udpateHeader tests"
+addClauseToHeaderTests = testGroup "addClauseToHeader tests"
+  [ testCase "should add 1 to the clause count" $
+    addClauseToHeader "p cnf 3 5" @?= "p cnf 3 6"
+  ]
+
+updateHeaderTests = testGroup "updateHeader tests"
   [ testCase "should update the clause count" $
-    updateHeader "p cnf 3 5" @?= "p cnf 3 6"
+    updateHeader 5 "p cnf 3 5" @?= "p cnf 3 10"
+  ]
+
+cnfsToLinesTests = testGroup "cnfsToLines tests"
+  [ testCase "should convert a single CNF to a string" $
+    cnfToLine [3, 4, 7] @?= "3 4 7 0"
+  , testCase "should convert multiple CNFs to a list of strings" $
+    cnfsToLines [[3, 4, 7], [1, 2, 3], [4, 5, 6]] @?= ["3 4 7 0", "1 2 3 0", "4 5 6 0"]
+  ]
+
+addClausesToCnfTests = testGroup "addClausesToCnf tests"
+  [ testCase "should append the clauses and update the header" $
+    let originalCnf = "p cnf 3 1\n" ++
+                      "1 2 3 0\n" in
+      let expected  = "p cnf 3 3\n" ++
+                      "1 2 3 0\n" ++
+                      "1 2 0\n" ++
+                      "3 2 0" in
+        addClausesToCnf originalCnf [[1, 2], [3, 2]] @?= expected
   ]
 
 parseCMSatSolutionTests = testGroup "parseCMSatSolution tests"
@@ -82,4 +113,11 @@ parseCMSatSolutionTests = testGroup "parseCMSatSolution tests"
     let output = "c Independent vars set: 1,2,3,4,5\n" ++
                  "s UNSATISFIABLE\n" in
       parseCMSatSolution output @?= []
+  ]
+
+isFormulaSatTests = testGroup "isFormulaSat tests"
+  [ testCase "should return true if SAT" $
+    isFormulaSat "s SATISFIABLE\nv 1 2 3 0" @?= True
+  , testCase "should return false if UNSAT" $
+    isFormulaSat "s UNSATISFIABLE\n" @?= False
   ]
